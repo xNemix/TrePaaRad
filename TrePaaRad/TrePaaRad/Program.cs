28b1c36460d9b6ca76e861edcedcc9f966958ca2
xNemix <46863616+xNemix@users.noreply.github.com>
2023-01-17 17:01:55 +0100

    Fancy greier

--- TrePaaRad/TrePaaRad/Program.cs
+++ TrePaaRad/TrePaaRad/Program.cs
@@ -2,47 +2,59 @@
 
 var board = new Board();
 var gameConsole = new GameConsole(board);
-string startNewGame = String.Empty;
+
+var isValidMark = true;
 while (gameConsole._gameIsRunning)
 {
-    if (board.HasPlayerWon(1))
-    {
-        Console.WriteLine("X vant spillet");
-        gameConsole._gameIsRunning = false;
-        Console.WriteLine("Vil du starte et nytt spill ?");
-        startNewGame = Console.ReadLine();
-        if (startNewGame.ToLower() == "ja")
-        {
-            board.RestartGame();
-        }
-    }
-    if (board.HasPlayerWon(2))
-    {
-        Console.WriteLine("O vant spillet");
-        gameConsole._gameIsRunning = false;
-        Console.WriteLine("Vil du starte et nytt spill ?");
-        startNewGame = Console.ReadLine();
-        if (startNewGame.ToLower() == "ja")
-        {
-            board.RestartGame();
-        }
-    }
-    gameConsole.Show();
-    Console.WriteLine("Skriv inn hvor du vil sette et kryss (f.eks \"a2\")");
-    var pos = Console.ReadLine()!;
-    var hasPlayerPlacedMarker = board.Mark(pos);
     
-    if (hasPlayerPlacedMarker)
+    if (board.HasPlayerWon(1) || board.HasPlayerWon(2))
     {
+        Console.Clear();
         gameConsole.Show();
-        Thread.Sleep(2000);
-        board.MarkRandom(false);
-        board.HasPlayerWon(2);
+        gameConsole._gameIsRunning = false;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        var gameWinner = board.HasPlayerWon(1) ? "Du" : "CPU-en";
+        Console.WriteLine($@"
+{gameWinner} vant spillet!
+Vil du starte et nytt spill ?");
+        Console.ForegroundColor = ConsoleColor.White;
+        var startNewGame = Console.ReadLine()!.ToLower();
+        if (startNewGame == "ja")
+        {
+            Console.Clear();
+            board.RestartGame();
+            isValidMark = true;
+        }
     }
-    else
+
+    if (isValidMark)
     {
-        Console.WriteLine("Du kan kun plassere dine brikker på en tom plass!");
         gameConsole.Show();
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("Skriv inn hvor du vil sette et kryss (f.eks \"a2\")");
+        Console.ForegroundColor = ConsoleColor.White;
+        var pos = Console.ReadLine()!;
+        var hasPlayerPlacedMarker = board.Mark(pos);
+        var hasPlayerWon = board.HasPlayerWon(1);
+        if (hasPlayerPlacedMarker && !hasPlayerWon)
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+            board.MarkRandom(false);
+            isValidMark = true;
+        }
+        else
+        {
+            isValidMark = false;
+        }
+    }
+    if (!isValidMark)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Du kan kun plassere brikken din på en tom plass!");
+        Console.ForegroundColor = ConsoleColor.White;
+        isValidMark = true;
     }
 }
 
